42d2853b11cd99ae8a78c3a16547a43c71f18d15
Orangel Gonzalez <goranjose16@gmail.com>
2022-04-07 07:48:37 -0400

    commit reservation

--- src/components/Reservation.tsx
+++ src/components/Reservation.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import imgAbout from "../assets/img/imgAbout.jpg"
-import "./Reservation.css"
+import imgReservation from "../assets/img/imgReservation.jpg"
 
 const Reservation = () => {
   return (
-  <section className="bg-[url('/src/assets/img/bgAbout.jpg')] bg-cover bg-bottom py-[70px] flex w-full h-auto justify-center">
+  <section className="bg-[url('/src/assets/img/bgReservation.jpg')] bg-cover bg-bottom py-[70px] flex w-full h-auto justify-center">
   		  <div className="px-[15px] font-[Rubik]  w-[570px] h-[438.75]">
   				  <h2 className='font-medium text-[30px] pb-[10px] leading-[120%]'>Welcome To <span className='text-[#d65106]'>Live Dinner Restaurant</span></h2>
   				  <h4 className='font-medium text-base pb-[10px] leading-[120%]'>Little Story</h4>
@@ -13,7 +12,7 @@ const Reservation = () => {
   				  <a className="absolute px-[30px] py-[12px] text-[16px] bg-[#d65106] text-[#ffffff] uppercase cursor-pointer font-[600] border-[#d65106] border-[2px] border-transparent rounded-[3px] hover:bg-[#333333] transition duration-[300ms] ease-in-out" href="#">Reservation</a>
   		  </div>
   		  <div className="px-[15px]">
-  			  <img src={imgAbout} alt="" className="w-[100%] h-[438.75px] shadow-[20px_20px_0px_0px_rgba(214,81,6,1)]" />
+  			  <img src={imgReservation} alt="" className="w-[100%] h-[438.75px] shadow-[20px_20px_0px_0px_rgba(214,81,6,1)]" />
   		  </div>
   </section>
     )
