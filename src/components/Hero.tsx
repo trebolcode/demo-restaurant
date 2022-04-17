f59184215cab6743186b4d2f5efe918cac523b57
Orangel Gonzalez <goranjose16@gmail.com>
2022-04-17 19:55:36 -0400

    sin el carousel infinito

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,30 +1,51 @@
 import React from "react";
-
-import ReserButton from "./subcomponents/ReserButton";
-import { Slide1, Slide2, Slide3 } from "./subcomponents/Slider";
+import Slider from "./subcomponents/Slider";
 import SliderLeft from "../assets/icon/chevron-left.svg";
 import SliderRight from "../assets/icon/chevron-right.svg";
+import "./Hero.css";
 
 const Hero = () => {
+  
+  // Codigo para js para cambiar el color de los botones
+  const punto = document.querySelectorAll(".punto");
+
+  punto.forEach((cadaPunto, i) => {
+    punto[i].addEventListener("click", () => {
+      punto.forEach((cadaPunto, i) => {
+        punto[i].classList.remove("activo");
+      });
+
+      punto[i].classList.add("activo");
+    });
+  });
+
   return (
     <main className="w-full h-screen flex justify-center items-end relative">
       <div
         id="slide1"
         className="w-full h-screen opacity-100 target:opacity-100 absolute transition-all ease-in-out duration-500"
       >
-        <Slide1 />
+        <div className="w-full h-screen bg-slider1 bg-cover bg-center absolute"></div>
+        <div className="w-full h-screen bg-colReserHover opacity-50 absolute"></div>
+        <Slider title="Live Dinner Restaurant" />
       </div>
+
       <div
         id="slide2"
         className="w-full h-screen opacity-0 target:opacity-100 absolute transition-all ease-in-out duration-500"
       >
-        <Slide2 />
+        <div className="w-full h-screen bg-slider2 bg-cover bg-center absolute"></div>
+        <div className="w-full h-screen bg-colReserHover opacity-50 absolute"></div>
+        <Slider title="Live Dinner Restaurant" />
       </div>
+
       <div
         id="slide3"
         className="w-full h-screen opacity-0 target:opacity-100 absolute transition-all ease-in-out duration-500"
       >
-        <Slide3 />
+        <div className="w-full h-screen bg-slider3 bg-cover bg-center absolute"></div>
+        <div className="w-full h-screen bg-colReserHover opacity-50 absolute"></div>
+        <Slider title="Yamifood Restaurant" />
       </div>
 
       <div className="my-0 m-auto absolute top-[46%] w-full">
@@ -43,18 +64,15 @@ const Hero = () => {
       </div>
 
       <nav className="w-full h-[49px] flex flex-nowrap justify-center space-x-1 absolute rounded-full">
-        <a
-          href="#slide1"
-          className="w-[12px] h-[45px] border-colPrimary rounded-full bg-colReser"
-        ></a>
-        <a
-          href="#slide2"
-          className="w-[12px] h-[45px] border-2 border-colPrimary rounded-full"
-        ></a>
-        <a
-          href="#slide3"
-          className="w-[12px] h-[45px] border-2 border-colPrimary rounded-full"
-        ></a>
+        <a href="#slide1">
+          <div className="w-[12px] h-[45px] border-2 border-colPrimary rounded-full punto activo"></div>
+        </a>
+        <a href="#slide2">
+          <div className="w-[12px] h-[45px] border-2 border-colPrimary rounded-full punto"></div>
+        </a>
+        <a href="#slide3">
+          <div className="w-[12px] h-[45px] border-2 border-colPrimary rounded-full punto"></div>
+        </a>
       </nav>
     </main>
   );
