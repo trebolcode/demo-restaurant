63848db247202b045173412b78bf76efc77bf9b9
Alejandro Ruiz <kannder.ziur@gmail.com>
2022-04-21 23:12:53 -0500

    correct gallery

--- src/components/subcomponents/ImageGrid.tsx
+++ src/components/subcomponents/ImageGrid.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const ImageGrid = ({ src_image }: Props) => {
   return (
-    <figure className="w-full border-4 border-primary-orange hover:scale-105 hover:border-transparent hover:drop-shadow-lg duration-300 ease-in-out transition-all cursor-pointer">
+    <figure className="w-full border-4 border-primary-orange hover:scale-105 hover:border-transparent hover:shadow-xl duration-300 ease-in-out transition-all cursor-pointer">
       <img className="w-full" src={src_image} alt="" />
     </figure>
   );
