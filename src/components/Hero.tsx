bea9ffc85c7efc4bf3c8d0095646ebd65d057811
Orangel Gonzalez <goranjose16@gmail.com>
2022-04-23 15:11:56 -0400

    componente Hero y subcomponente Slider

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,79 +1,110 @@
-import React from "react";
+import React, { useRef } from "react";
 import Slider from "./subcomponents/Slider";
+import ReserButton from "./subcomponents/ReserButton";
 import SliderLeft from "../assets/icon/chevron-left.svg";
 import SliderRight from "../assets/icon/chevron-right.svg";
-import "./Hero.css";
 
 const Hero = () => {
-  
-  // Codigo para js para cambiar el color de los botones
-  const punto = document.querySelectorAll(".punto");
+  // Codigo para Carousel
+  const slideshow: any = useRef();
 
-  punto.forEach((cadaPunto, i) => {
-    punto[i].addEventListener("click", () => {
-      punto.forEach((cadaPunto, i) => {
-        punto[i].classList.remove("activo");
-      });
+  const nextSlide = () => {
+    if (slideshow.current.children.length > 0) {
+      const firstElement = slideshow.current.children[0];
 
-      punto[i].classList.add("activo");
-    });
-  });
+      slideshow.current.style.transition = "100ms ease-in-out all";
+
+      const sizeSlide = slideshow.current.children[0].offsetWidth;
+
+      slideshow.current.style.transform = `translateX(-${sizeSlide}px)`;
+
+      const transicion = () => {
+        slideshow.current.style.transition = "none";
+        slideshow.current.style.transform = "translateX(0)";
+        slideshow.current.appendChild(firstElement);
+        slideshow.current.removeEventListener("transitionend", transicion);
+      };
+
+      slideshow.current.addEventListener("transitionend", transicion);
+    }
+  };
+
+  const previousSlide = () => {
+    console.log("previousSlide");
+    if (slideshow.current.children.length > 0) {
+      const index = slideshow.current.children.length - 1;
+      const lastElement = slideshow.current.children[index];
+
+      slideshow.current.insertBefore(lastElement, slideshow.current.firstChild);
+
+      slideshow.current.style.transition = "none";
+
+      const sizeSlide = slideshow.current.children[0].offsetWidth;
+
+      slideshow.current.style.transform = `translateX(-${sizeSlide}px)`;
+
+      setTimeout(() => {
+        slideshow.current.style.transition = "10ms ease-in-out all";
+        slideshow.current.style.transform = `translateX(0)`;
+      }, 0);
+
+      const transicion = () => {
+        slideshow.current.style.transition = "none";
+        slideshow.current.style.transform = "translateX(0)";
+        slideshow.current.insertBefore(
+          lastElement,
+          slideshow.current.children[0]
+        );
+      };
+
+      slideshow.current.addEventListener("transitionend", transicion);
+    }
+  };
 
   return (
     <main className="w-full h-screen flex justify-center items-end relative">
       <div
-        id="slide1"
-        className="w-full h-screen opacity-100 target:opacity-100 absolute transition-all ease-in-out duration-500"
+        ref={slideshow}
+        className="w-full h-screen flex flex-row justify-center items-end flex-nowrap"
       >
-        <div className="w-full h-screen bg-slider1 bg-cover bg-center absolute"></div>
-        <div className="w-full h-screen bg-colReserHover opacity-50 absolute"></div>
-        <Slider title="Live Dinner Restaurant" />
+        <div id="slide1" className="w-full h-screen absolute">
+          <div className="w-full h-screen bg-slider1 bg-cover bg-center absolute"></div>
+          <div className="w-full h-screen bg-colReserHover opacity-50 absolute"></div>
+          <Slider title="Live Dinner Restaurant" />
+        </div>
+
+        <div id="slide2" className="w-full h-screen absolute">
+          <div className="w-full h-screen bg-slider2 bg-cover bg-center absolute"></div>
+          <div className="w-full h-screen bg-colReserHover opacity-50 absolute"></div>
+          <Slider title="Live Dinner Restaurant" />
+        </div>
+
+        <div id="slide3" className="w-full h-screen absolute">
+          <div className="w-full h-screen bg-slider3 bg-cover bg-center absolute"></div>
+          <div className="w-full h-screen bg-colReserHover opacity-50 absolute"></div>
+          <Slider title="Yamifood Restaurant" />
+        </div>
       </div>
 
-      <div
-        id="slide2"
-        className="w-full h-screen opacity-0 target:opacity-100 absolute transition-all ease-in-out duration-500"
-      >
-        <div className="w-full h-screen bg-slider2 bg-cover bg-center absolute"></div>
-        <div className="w-full h-screen bg-colReserHover opacity-50 absolute"></div>
-        <Slider title="Live Dinner Restaurant" />
-      </div>
-
-      <div
-        id="slide3"
-        className="w-full h-screen opacity-0 target:opacity-100 absolute transition-all ease-in-out duration-500"
-      >
-        <div className="w-full h-screen bg-slider3 bg-cover bg-center absolute"></div>
-        <div className="w-full h-screen bg-colReserHover opacity-50 absolute"></div>
-        <Slider title="Yamifood Restaurant" />
-      </div>
-
-      <div className="my-0 m-auto absolute top-[46%] w-full">
-        <a
-          href="#slide1"
-          className="absolute left-0 bg-colReser w-[50px] h-[50px] flex justify-center items-center rounded-[4px] hover:bg-colSliderHover transition duration-[300ms] ease-in-out"
+      <div className="my-0 m-auto top-[54%] lg:top-[46%] w-full absolute">
+        <button
+          onClick={previousSlide}
+          className="absolute left-0 bg-colReser w-10 lg:w-12 h-10 lg:h-12 flex justify-center items-center rounded-[4px] lg:hover:bg-colSliderHover transition duration-[300ms] ease-in-out"
         >
           <img src={SliderLeft} className="h-8 w-8" />
-        </a>
-        <a
-          href="#slide2"
-          className="absolute right-0 bg-colReser w-[50px] h-[50px] flex justify-center items-center rounded-[4px] hover:bg-colSliderHover transition duration-[300ms] ease-in-out"
+        </button>
+
+        <button
+          onClick={nextSlide}
+          className="absolute right-0 bg-colReser w-10 lg:w-12 h-10 lg:h-12 flex justify-center items-center rounded-[4px] lg:hover:bg-colSliderHover transition duration-[300ms] ease-in-out"
         >
           <img src={SliderRight} className="h-8 w-8" />
-        </a>
+        </button>
       </div>
 
-      <nav className="w-full h-[49px] flex flex-nowrap justify-center space-x-1 absolute rounded-full">
-        <a href="#slide1">
-          <div className="w-[12px] h-[45px] border-2 border-colPrimary rounded-full punto activo"></div>
-        </a>
-        <a href="#slide2">
-          <div className="w-[12px] h-[45px] border-2 border-colPrimary rounded-full punto"></div>
-        </a>
-        <a href="#slide3">
-          <div className="w-[12px] h-[45px] border-2 border-colPrimary rounded-full punto"></div>
-        </a>
-      </nav>
+      <div className="top-[70%] left-[6%] lg:top-[64%] lg:left-[6%] absolute font-[600] font-Rubik">
+        <ReserButton name_button="Reservation" />
+      </div>
     </main>
   );
 };
--- src/components/subcomponents/Slider.tsx
+++ src/components/subcomponents/Slider.tsx
@@ -9,17 +9,16 @@ function Slider({ title }: Props) {
   return (
     <>
       <div className="w-full h-screen flex justify-center items-center absolute">
-        <div className="w-[1140px] h-auto px-[15px] pb-4 font-Rubik ">
-          <h1 className="text-[64px] font-[500] text-colPrimary pb-[10px] leading-[120%]">
+        <div className="w-full lg:w-full h-auto px-4 md:px-10 lg:px-20 pb-32 lg:pb- font-Rubik ">
+          <h1 className="text-[24px] md:text-[48px] lg:text-[64px] font-[500] text-colPrimary lg:pb-[10px] leading-[120%]">
             <strong>
               Welcome To <br /> {title}
             </strong>
           </h1>
-          <p className="text-[18px] text-colPrimary mb-[16px] leading-[1.8]">
+          <p className="text-[14px] md:text-[16px] lg:text-[18px] text-colPrimary mb-[16px] leading-[1.8]">
             See how your users experience your website in realtime or view{" "}
             <br /> trends to see any changes in performance over time.
           </p>
-          <ReserButton name_button="Reservation" />
         </div>
       </div>
     </>
