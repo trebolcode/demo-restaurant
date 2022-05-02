3144360476b7d9d8eed104d329163ba714de5ec7
Orangel Gonzalez <goranjose16@gmail.com>
2022-05-02 10:22:16 -0400

    Fix Component Hero and Slider

--- src/components/subcomponents/Slider.tsx
+++ src/components/subcomponents/Slider.tsx
@@ -9,7 +9,7 @@ function Slider({ title }: Props) {
   return (
     <>
       <div className="w-full h-screen flex justify-center items-center absolute">
-        <div className="w-full lg:w-full h-auto px-4 md:px-10 lg:px-20 pb-32 lg:pb- font-Rubik ">
+        <div className="w-full lg:w-full h-auto px-4 md:px-10 lg:px-20 pb-32 font-Rubik ">
           <h1 className="text-[24px] md:text-[48px] lg:text-[64px] font-[500] text-colPrimary lg:pb-[10px] leading-[120%]">
             <strong>
               Welcome To <br /> {title}
