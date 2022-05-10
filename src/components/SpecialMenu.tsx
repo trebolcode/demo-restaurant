e22e2580a1b7780d8cc3159d3393182c2d23f237
Alejandro Ruiz <kannder.ziur@gmail.com>
2022-05-09 20:12:03 -0500

    new grid

--- src/components/SpecialMenu.tsx
+++ src/components/SpecialMenu.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Cards from "./subcomponents/Cards";
+import images from "../assets/images";
 
 const SpecialMenu = () => {
   return (
-    <section>SpecialMenu</section>
-  )
-}
+    <section className="">
+      <h2>Special Menu</h2>
+      <h3>Lorem Ipsum is simply dummy text of the printing and typesetting</h3>
+      <div className="w-full flex flex-1 flex-3">
+        <div>left bar</div>
+        <div className="w-full h-96 grid gap-2 grid-cols-3 grid-rows-3">
+          {images.grid_image.map((image) => (
+            <Cards
+              subtitle={image.title}
+              description={image.description}
+              price={image.price}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default SpecialMenu
+export default SpecialMenu;
--- src/components/subcomponents/Cards.tsx
+++ src/components/subcomponents/Cards.tsx
@@ -1,45 +1,31 @@
 import React from "react";
 
-const Cards = () => {
+interface Props {
+  subtitle: string;
+  description: string;
+  price: number;
+}
+
+const defaultProps: Props = {
+  subtitle: "Special Drinks",
+  description: "Sed id magna vitae eros sagittis euismod.",
+  price: 7.0,
+};
+
+const Cards = ({ subtitle, description, price }: Props) => {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <article className="w-96 h-96 bg-slider1 bg-cover bg-center overflow-hidden cursor-pointer rounded-xl  group">
-        <div className="transform-gpu w-full h-full flex flex-col justify-end items-center   border-t-2 border-t-black opacity-0 group-hover:opacity-100 group-hover:bg-red-300/80 translate-y-full group-hover:translate-y-0 transition duration-500 ease-in-out">
-          <p>This is one text</p>
-          <p>This is two text</p>
-          <p>This is three text</p>
-        </div>
-      </article>
-      <article className="w-96 h-96 bg-slider1 bg-cover bg-center overflow-hidden cursor-pointer rounded-xl  group">
-        <div className="transform-gpu w-full h-full flex flex-col justify-end items-center   border-t-2 border-t-black opacity-0 group-hover:opacity-100 group-hover:bg-red-300/80 translate-y-full group-hover:translate-y-0 transition duration-500 ease-in-out">
-          <p>This is one text</p>
-          <p>This is two text</p>
-          <p>This is three text</p>
-        </div>
-      </article>
-      <article className="w-96 h-96 bg-slider1 bg-cover bg-center overflow-hidden cursor-pointer rounded-xl  group">
-        <div className="transform-gpu w-full h-full flex flex-col justify-end items-center   border-t-2 border-t-black opacity-0 group-hover:opacity-100 group-hover:bg-red-300/80 translate-y-full group-hover:translate-y-0 transition duration-500 ease-in-out">
-          <p>This is one text</p>
-          <p>This is two text</p>
-          <p>This is three text</p>
-        </div>
-      </article>
-      <article className="w-96 h-96 bg-slider1 bg-cover bg-center overflow-hidden cursor-pointer rounded-xl  group">
-        <div className=" w-full h-full flex flex-col justify-end items-center   border-t-2 border-t-black opacity-0 group-hover:opacity-100 group-hover:bg-red-300/80 translate-y-full group-hover:translate-y-0 transition duration-500 ease-in-out">
-          <p>This is one text</p>
-          <p>This is two text</p>
-          <p>This is three text</p>
-        </div>
-      </article>
-      <article className="w-96 h-96 bg-slider1 bg-cover bg-center overflow-hidden cursor-pointer rounded-xl  group">
-        <div className=" w-full h-full flex flex-col justify-end items-center   border-t-2 border-t-black opacity-0 group-hover:opacity-100 group-hover:bg-red-300/80 translate-y-full group-hover:translate-y-0 transition duration-500 ease-in-out">
-          <p>This is one text</p>
-          <p>This is two text</p>
-          <p>This is three text</p>
+    <div className="w-full  flex justify-center items-center">
+      <article className="w-full h-full bg-slider1 bg-cover bg-center overflow-hidden cursor-pointer rounded-xl group">
+        <div className="transform-gpu w-full h-full flex flex-col justify-end items-center  border-t-2 border-t-black opacity-0 group-hover:opacity-100 group-hover:bg-red-300/80 translate-y-full group-hover:translate-y-0 transition duration-500 ease-in-out">
+          <p>{subtitle}</p>
+          <p>{description}</p>
+          <p>{price}</p>
         </div>
       </article>
     </div>
   );
 };
+
+Cards.defaultProps = defaultProps;
 
 export default Cards;
